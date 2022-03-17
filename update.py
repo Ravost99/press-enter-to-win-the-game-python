5515fb1e4dbd36d75c67234a0b61e231986f2c9d
Daniel Z <75264908+Ravost99@users.noreply.github.com>
2022-03-17 21:19:36 +0000

    updates.py

--- update.py
+++ update.py
@@ -3,7 +3,7 @@ import urllib
 def update():
     stuff_to_update = ['main.py', '_def.py']
     for fl in stuff_to_update:
-        dat = urllib.request.urlopen("https://raw.githubusercontent.com/Ravost99/Disnake.py-Bot-Template/master/" + fl).read()
+        dat = urllib.request.urlopen("https://raw.githubusercontent.com/Ravost99/press-enter-to-win-the-game-python/main/" + fl).read()
         with open(fl, 'wb') as file:
           file.write(dat)
     print('\n\t\tUpdated Successfull !!!!')
